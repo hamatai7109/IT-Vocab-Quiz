86829eadfe6bd1cc0897fdb5515be532e67673b3
taishi-hamano <>
2024-06-08 13:08:54 +0900

    create the basement of Github authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div>toppage</div>;
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { AuthButton } from "@/components/AuthButton";
+
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies });
+  const { data: posts } = await supabase.from("posts").select();
+
+  return (
+    <>
+      <AuthButton />
+      <pre>{JSON.stringify(posts, null, 2)}</pre>;
+    </>
+  );
 }
