166900240524994f29a63f73282dbdfc394da296
taishi-hamano <>
2024-06-08 16:28:02 +0900

    Complete Email authentication through supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+
 import { cookies } from "next/headers";
 import GithubAuthButtonServer from "@/components/auth/github/GithubAuthButtonServer";
+import EmailAuth from "@/components/auth/email/EmailAuth";
+import { useRouter } from "next/navigation";
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
@@ -8,6 +11,8 @@ export default async function Home() {
 
   return (
     <>
+      <h1>Supabase + Nextjs Auth App</h1>
+      <EmailAuth />
       <GithubAuthButtonServer />
       <pre>{JSON.stringify(posts, null, 2)}</pre>;
     </>
